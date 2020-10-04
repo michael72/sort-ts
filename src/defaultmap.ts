50cf033df5ad94c0cd1f6ae13339f89c2520d0dd
Michael Schulte <michael.schulte@gmx.org>
2020-10-04 20:12:36 +0200

    improve documentation

--- src/defaultmap.ts
+++ src/defaultmap.ts
@@ -1,33 +1,28 @@
 /**
- * Map class that with a getter function that returns the
- * default value given by the callback function used in the
- * constructor.
+ * Map class with a callback function that creates a default value
+ * instead of returning an undefined value.
  * A default value can not be used as it would always return
  * the same reference.
  */
-// eslint-disable-next-line no-undef
 export class DefaultMap<K, V> extends Map<K, V> {
   /** Saves the default value callback */
-  private defaultVal: () => V;
-  public constructor(defaultVal: () => V) {
+  public constructor(private defaultVal: () => V) {
     super();
-    this.defaultVal = defaultVal;
   }
-  /** Gets the value for the given key or the default value,
-   * if none was yet added. Also adds the default value to the map.
+  /** Gets the value for the given key or the default value
+   * if none was yet added. Adds the default value to the map.
    * In comparison to get() it also returns the actual type, not undefined.
-   * @param key the search key to find its value.
-   * @returns the value - either the saved or default.
+   * @param key the search key to lookup the value.
+   * @returns the value - either the saved or newly created default.
    */
   public getDef(key: K): V {
     const v = super.get(key);
-    if (v === undefined) {
-      const newValue = this.defaultVal();
-      super.set(key, newValue);
-      return newValue;
-    } else {
-      // value was already added
-      return v;
-    }
+    return v === undefined ? this._createDefault(key) : v;
+  }
+
+  private _createDefault(key: K) {
+    const newValue = this.defaultVal();
+    super.set(key, newValue);
+    return newValue;
   }
 }
